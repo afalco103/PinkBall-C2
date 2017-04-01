554913a4547b1d1222f864b6383546675fbb1736
afalco103 <afalco103@gmail.com>
2017-04-01 17:16:32 -0400

    added background

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1491079555,
+	"version": 1491081358,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -16,6 +16,7 @@
 		"images/bullet-sheet0.png",
 		"images/enemy-sheet0.png",
 		"images/combocoin-sheet0.png",
+		"images/stargun.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
