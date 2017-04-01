1439eba76432369cc894236afd5dd938ba00963d
afalco103 <afalco103@gmail.com>
2017-04-01 16:49:47 -0400

    exported to html5 playable version

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1491079555,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/pipe-sheet0.png",
+		"images/pipe-sheet1.png",
+		"images/pipegate-sheet0.png",
+		"images/pipesides-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/powerup-sheet0.png",
+		"images/powerup-sheet1.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/combocoin-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
